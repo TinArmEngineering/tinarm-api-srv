8de84c7e28576939404791a4eb7d2ff867fc1cd5
Chris Wallis <chris@inybble.com>
2023-01-11 20:08:28 +0000

    Added starter for 10 on standard unit lookup

--- go/services/unitssvc.go
+++ go/services/unitssvc.go
@@ -30,6 +30,74 @@ type UnitError struct {
 }
 
 type UnitsSvc struct {
+	unitMap  map[string][]Unit
+	fieldMap map[string]string
+}
+
+func (unitsSvc UnitsSvc) getMaps() (map[string]string, map[string][]Unit) {
+
+	if len(unitsSvc.unitMap) == 0 {
+
+		unitsSvc.unitMap = make(map[string][]Unit)
+
+		unitsSvc.unitMap["displacement"] = []Unit{{Name: "m", Exponent: 1}}
+		unitsSvc.unitMap["mass"] = []Unit{{Name: "kg", Exponent: 1}}
+		unitsSvc.unitMap["area"] = []Unit{{Name: "m", Exponent: 2}}
+		unitsSvc.unitMap["volume"] = []Unit{{Name: "m", Exponent: 3}}
+		unitsSvc.unitMap["force"] = []Unit{{Name: "N", Exponent: 1}}
+
+		unitsSvc.unitMap["pressure"] = []Unit{
+			{Name: "N", Exponent: 1},
+			{Name: "m", Exponent: -2}}
+
+		unitsSvc.unitMap["volumetric_mass_density"] = []Unit{
+			{Name: "kg", Exponent: 1},
+			{Name: "m", Exponent: -3}}
+
+		unitsSvc.unitMap["thermal_conductivity"] = []Unit{
+			{Name: "W", Exponent: 1},
+			{Name: "m", Exponent: -1},
+			{Name: "K", Exponent: -1}}
+
+		unitsSvc.unitMap["electrical_conductivity"] = []Unit{
+			{Name: "S", Exponent: 1},
+			{Name: "m", Exponent: -1}}
+
+		unitsSvc.unitMap["magnetic_permeability"] = []Unit{
+			{Name: "H", Exponent: 1},
+			{Name: "m", Exponent: -1}}
+
+		unitsSvc.unitMap["permittivity"] = []Unit{
+			{Name: "F", Exponent: 1},
+			{Name: "m", Exponent: -1}}
+
+		unitsSvc.unitMap["heat_capacity"] = []Unit{
+			{Name: "J", Exponent: 1},
+			{Name: "K", Exponent: -1}}
+
+		unitsSvc.fieldMap = make(map[string]string)
+
+		// Fields from Material
+		unitsSvc.fieldMap["width"] = "displacement"
+		unitsSvc.fieldMap["depth"] = "displacement"
+		unitsSvc.fieldMap["depth"] = "displacement"
+		unitsSvc.fieldMap["heat_conductivity"] = "thermal_conductivity"
+		unitsSvc.fieldMap["electric_conductivity"] = "electrical_conductivity"
+		unitsSvc.fieldMap["relative_permeability"] = "magnetic_permeability"
+		unitsSvc.fieldMap["relative_permittivity"] = "permittivity"
+		unitsSvc.fieldMap["heat_capacity"] = "heat_capacity"
+		unitsSvc.fieldMap["density"] = "volumetric_mass_density"
+	}
+
+	return unitsSvc.fieldMap, unitsSvc.unitMap
+}
+
+func (unitsSvc UnitsSvc) StandardFieldUnits(fieldName string) []Unit {
+	fieldMap, unitMap := unitsSvc.getMaps()
+	if val, ok := fieldMap[fieldName]; ok {
+		return unitMap[val]
+	}
+	return []Unit{}
 }
 
 // Converts a quantity of whatever into an equivalent quantity of SI units
@@ -45,12 +113,7 @@ type UnitsSvc struct {
 //	}
 func (unitsSvc UnitsSvc) ConvertToBaseUnits(qty Quantity) (Quantity, error) {
 
-	json_data, err := json.Marshal(qty)
-	if err != nil {
-		return Quantity{}, err
-	}
-
-	return callUnitsService("convert_to_base_units", json_data)
+	return callUnitsService("convert_to_base_units", qty)
 }
 
 // Checks that the specified 'from' Quantity can be converted to the
@@ -67,17 +130,17 @@ func (unitsSvc UnitsSvc) ConvertToBaseUnits(qty Quantity) (Quantity, error) {
 //	}
 func (unitsSvc UnitsSvc) ConvertTo(from Quantity, to Quantity) (Quantity, error) {
 
-	json_data, err := json.Marshal(ForceQuantity{From: from, To: to})
+	return callUnitsService("convert_to", ForceQuantity{From: from, To: to})
+}
+
+func callUnitsService(method string, v any) (Quantity, error) {
+
+	result := Quantity{}
+
+	json_data, err := json.Marshal(v)
 	if err != nil {
 		return Quantity{}, err
 	}
-
-	return callUnitsService("convert_to", json_data)
-}
-
-func callUnitsService(method string, json_data []byte) (Quantity, error) {
-
-	res := Quantity{}
 
 	resp, err := http.Post(
 		"http://"+env.UnitsServiceHost()+"/"+method,
@@ -94,5 +157,5 @@ func callUnitsService(method string, json_data []byte) (Quantity, error) {
 		return Quantity{}, fmt.Errorf("%v %v %v", resp.Status, unitError.Message, unitError.Type)
 	}
 
-	return res, json.NewDecoder(resp.Body).Decode(&res)
+	return result, json.NewDecoder(resp.Body).Decode(&result)
 }
